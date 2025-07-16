90b54528d8666e0e8cf3f55f87e5c78abb125339
AxisMediatv <axismediatv@gmail.com>
2025-07-16 11:40:28 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -179,12 +179,15 @@ Remember, even small acts of kindness can make a huge difference in someone's da
   useEffect(() => {
     if (messages.length === 0) {
       const greetings = [
-        "Hi, I missed you. How have you been?",
-        "Hey, it's good to hear from you",
-        "Let's have some one-on-one time to talk",
-        "I'm all ears, let it all out",
-        "It's great to be here with you. What's going on?"
-      ];
+    const greetings = [
+  "Hi, I missed you. How have you been?",
+  "Hi there! How's it going? I would love to hear more. Remember, I am here for you",
+  "Hey, how are you feeling today? I want to take some time to just listen to you. Tell me what's going on",
+  "Hey! Talk to me - what's going on?",
+  "Hey, it's good to hear from you. Let's have some one-on-one time to talk",
+  "I'm all ears, let it all out. What's on your mind?",
+  "It's great to be here with you. Do you have a few moments to tell me what's going on?"
+];
       const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
       setMessages([{
         role: 'assistant',
